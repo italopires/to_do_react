410dc17c4fd9426290c9ad48d8544708b7995dcc
Italo Pires <italonapalm@gmail.com>
2019-07-20 20:52:18 -0300

    Added pageHeader

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
+import PageHeader from '../template/pageHeader'
+
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name="Sobre" small="Nós"></PageHeader>
+
+        <h2>Nossa História</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores quidem impedit facilis natus. Alias explicabo, eaque maiores quibusdam recusandae natus odit autem praesentium neque sint reprehenderit dolorem velit magni tenetur.</p>
+        <h2>Missão e Visão</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde dolorem odio consequuntur dicta beatae, placeat quia aspernatur rem repudiandae quis! Itaque harum libero sint explicabo? Consequatur repudiandae molestias quibusdam libero?</p>
+        <h2>Imprensa</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque in voluptatum nobis officia, minima, reiciendis incidunt quam quia quis provident, esse recusandae reprehenderit consequatur assumenda ipsa aut! Quisquam, sed rerum.</p>
     </div>
 )
